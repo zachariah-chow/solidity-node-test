cadfddcb23cdf769b3ae099f7375d74765de88aa
Zachariah Chow <zachariahchow@gmail.com>
2020-09-23 01:03:44 +0800

    test: Add dummy logic to test deployment and ganache accounts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,33 @@
+//Bug in Ganache CLI: only works with node versions 12 and below
+
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    // Use one account to deploy contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ['Test arg'],
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000',
+        });
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
