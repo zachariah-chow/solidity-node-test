ca24f1677d3a7d7617383a9b51db2a7916811d93
Zachariah Chow <zachariahchow@gmail.com>
2020-09-23 01:19:02 +0800

    fix: Hotfix for Web3 broken packages

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,7 +4,9 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -24,6 +26,8 @@ beforeEach(async () => {
             from: accounts[0],
             gas: '1000000',
         });
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
